deb21b73a073e4e2de740883f6ac1b81b8d3083b
Nicolas Barbey <223106@supinfo.com>
2019-07-31 14:46:36 +0200

    Add labels

--- I18n/backOffice/default/fr_FR.php
+++ I18n/backOffice/default/fr_FR.php
@@ -2,4 +2,8 @@
 
 return array(
     'Save' => 'Enregistrer',
+    'noindex.help' => 'Permet de ne pas indexer la page Gestion de la meta robots noindex, nofollow',
+    'canonical.help' => 'Permet de définir l\'url principale (canonique)',
+    'label.noindex' => 'Gestion de la meta robots noindex, nofollow :',
+    'canonical' => 'Url Canonique',
 );

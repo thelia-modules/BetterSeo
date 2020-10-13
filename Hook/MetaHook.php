e0e807a9738a95209dc08960a215ead3bfb74d5f
Nicolas Barbey <223106@supinfo.com>
2020-10-13 13:33:20 +0200

    Add hook for front office

---
+++ Hook/MetaHook.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: nicolasbarbey
+ * Date: 14/09/2020
+ * Time: 15:10
+ */
+
+namespace BetterSeo\Hook;
+
+
+use Thelia\Core\Event\Hook\HookRenderEvent;
+use Thelia\Core\Hook\BaseHook;
+use Thelia\Core\HttpFoundation\Request;
+
+class MetaHook extends BaseHook
+{
+    protected $request;
+
+    public function __construct(Request $request)
+    {
+        $this->request = $request;
+    }
+
+    public function getRequest()
+    {
+        return $this->request;
+    }
+
+    public function onMainHeadBottom(HookRenderEvent $event)
+    {
+        $request = $this->getRequest();
+
+        if($view = $request->get('_view')) {
+
+            $id = $request->get($view . '_id');
+
+            $lang = $request->getSession()->getLang();
+
+            $event->add(
+                $this->render('meta_hook.html', [
+                    'object_id' => $id,
+                    'object_type' => $view,
+                    'lang_id' => $lang->getId()
+                ])
+            );
+        }
+    }
+}

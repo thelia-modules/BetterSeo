ecd47c6beccddedec10915f2563620834672cbce
Alexandre Nozière <anoziere@openstudio.fr>
2026-06-09 11:32:10 +0200

    fix: use Thelia\Core\Install\Database namespace in postActivation

--- BetterSeo.php
+++ BetterSeo.php
@@ -22,7 +22,7 @@ namespace BetterSeo;
 use Propel\Runtime\Connection\ConnectionInterface;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ServicesConfigurator;
 use Symfony\Component\Finder\Finder;
-use Thelia\Install\Database;
+use Thelia\Core\Install\Database;
 use Thelia\Module\BaseModule;
 
 class BetterSeo extends BaseModule
